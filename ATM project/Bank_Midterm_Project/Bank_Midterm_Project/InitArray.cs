3349e9386e4722be93dd5b58a24df8e020e1f6f1
Ryan Brooks <ryan@experimentalthoughts.com>
2016-09-29 17:34:59 -0500

    Cleared
    Commented this out so it doesn't interfere with anthing else.

--- ATM project/Bank_Midterm_Project/Bank_Midterm_Project/InitArray.cs
+++ ATM project/Bank_Midterm_Project/Bank_Midterm_Project/InitArray.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace Bank_Midterm_Project  // This is the example from class, as an intro to arrays.
 {
-    class InitArray
-    {
-        public static void Main(string[] args)
-        {
-            int[] array;
-            array = new int[5];
-            Console.WriteLine("{1}{1,8}", "Index", "Value");
-            for (int counter = 0; counter < array.Length; ++counter)
-               {
-                Console.WriteLine("{0,5}{1,8}", counter, array[counter]);
-                }
-            Console.ReadLine();
-        }
-    }
+  //  class InitArray
+    //{
+      //  public static void Main(string[] args)
+       // {
+         //   int[] array;
+           // array = new int[5];
+           // Console.WriteLine("{1}{1,8}", "Index", "Value");
+           // for (int counter = 0; counter < array.Length; ++counter)
+             //  {
+              //  Console.WriteLine("{0,5}{1,8}", counter, array[counter]);
+               // }
+           // Console.ReadLine();
+      //  }
+  //  }
 }
